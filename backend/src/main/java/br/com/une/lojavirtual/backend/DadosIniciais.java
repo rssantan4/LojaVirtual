32d16286b2714e3742b24f277e0d6ca7ac439702
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-12-12 11:04:56 -0300

    Update DadosIniciais.java
    ajeitando a versão

--- backend/src/main/java/br/com/une/lojavirtual/backend/DadosIniciais.java
+++ backend/src/main/java/br/com/une/lojavirtual/backend/DadosIniciais.java
@@ -1082,7 +1082,7 @@ Produto p94 = new Produto(
     new BigDecimal("199.90"),
     150,
     kpop,
-    "assets/img/fotos-produtos/produto94.jpg"
+    "assets/img/fotos-produtos/produto94.jpeg"
 );
 
 Produto p95 = new Produto(
@@ -1093,7 +1093,7 @@ Produto p95 = new Produto(
     new BigDecimal("155.00"),
     120,
     kpop,
-    "assets/img/fotos-produtos/produto95.jpg"
+    "assets/img/fotos-produtos/produto95.jpeg"
 );
 
 Produto p96 = new Produto(
@@ -1104,7 +1104,7 @@ Produto p96 = new Produto(
     new BigDecimal("245.00"),
     14,
     kpop,
-    "assets/img/fotos-produtos/produto96.jpg"
+    "assets/img/fotos-produtos/produto96.jpeg"
 );
 
 Produto p97 = new Produto(
@@ -1115,7 +1115,7 @@ Produto p97 = new Produto(
     new BigDecimal("89.90"),
     180,
     worship,
-    "assets/img/fotos-produtos/produto97.jpg"
+    "assets/img/fotos-produtos/produto97.jpeg"
 );
 
 Produto p98 = new Produto(
@@ -1126,7 +1126,7 @@ Produto p98 = new Produto(
     new BigDecimal("209.90"),
     200,
     pop,
-    "assets/img/fotos-produtos/produto98.jpg"
+    "assets/img/fotos-produtos/produto98.jpeg"
 );
 
 Produto p99 = new Produto(
@@ -1137,7 +1137,7 @@ Produto p99 = new Produto(
     new BigDecimal("209.90"),
     150,
     pop,
-    "assets/img/fotos-produtos/produto99.jpg"
+    "assets/img/fotos-produtos/produto99.jpeg"
 );
 
 Produto p100 = new Produto(
@@ -1148,7 +1148,7 @@ Produto p100 = new Produto(
     new BigDecimal("89.90"),
     18,
     worship,
-    "assets/img/fotos-produtos/produto100.jpg"
+    "assets/img/fotos-produtos/produto100.jpeg"
 );
 
                produtoRepo.saveAll(Arrays.asList( p1, p2, p3, p4, p5,p6, p7, p8, p9, p10,p11, p12, p13, p14, p15,p16, p17, p18, p19, p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30, p31, p32, p33, p34,p35, p36,p37,p38,p39,p40,p41,p42,p43,p44,p45,p46, p47, p48, p49, p50, p51, p52, p53, p54, p55,p56, p57, p58, p59, p60,p61, p62, p63, p64, p65,p66, p67, p68, p69, p70, p71, p72, p73,p74,p75, p76, p77, p78,p79, p80, p81, p82,p83, p84, p85, p86, p87,p88, p89, p90,p91,p92,p93,p94,p95,p96,p97,p98,p99,p100));
