a7c2abf594b94bc9bbdca27e3c01c22ffb67b27a
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-12-12 13:13:10 -0300

    Produto card
    aparecer as informações dos produtos e as sugestões de outras compras para o cliente.

--- frontend/src/app/produto-info/produto-info.ts
+++ frontend/src/app/produto-info/produto-info.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProdutoService } from '../Area-Adm/produtos/services/produto-service';
+import { Produto } from '../models/produto-model';
+import { CardProdutoLoja } from '../loja/card-produto-loja/card-produto-loja';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produto-info',
-  imports: [],
+  imports: [CardProdutoLoja,CommonModule],
   templateUrl: './produto-info.html',
   styleUrl: './produto-info.scss',
 })
 export class ProdutoInfo {
+   @ViewChild('carousel', { static: false }) carousel!: ElementRef<HTMLDivElement>;
+
+  produto!: Produto;
+  produtosRelacionados: Produto[] = [];
+
+  constructor(
+    private route: ActivatedRoute,        // Para pegar o id na URL
+    private produtoService: ProdutoService, // Serviço para buscar os dados
+  ) {}
+
+ngOnInit() {
+  this.route.paramMap.subscribe(params => {
+    const produtoId = Number(params.get('id'));
+
+    // 1️⃣ Buscar produto atual
+    this.produtoService.getProdutoById(produtoId).subscribe(prod => {
+      if (prod) {
+        this.produto = prod;
+
+        // 2️⃣ Buscar produtos relacionados pelo mesmo gênero
+        this.produtoService.getByCategoria(String(this.produto.generoMusical.nome))
+          .subscribe(produtos => {
+            this.produtosRelacionados = produtos.filter(p => p.id !== this.produto.id);
+          });
+      }
+    });
+  });
+}
+
+
+  scroll(direction: number) {
+    if (this.carousel) {
+      const width = this.carousel.nativeElement.offsetWidth;
+      this.carousel.nativeElement.scrollBy({ left: direction * width / 2, behavior: 'smooth' });
+    }
+  }
+
+
 
 }
