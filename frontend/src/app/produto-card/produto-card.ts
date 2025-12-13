ce3985d849f4e2a415dfe9d8ce61fd28621ea064
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-12-13 11:08:41 -0300

    Card Home
    Vou deixar assim por enquanto.

--- frontend/src/app/produto-card/produto-card.ts
+++ frontend/src/app/produto-card/produto-card.ts
@@ -3,6 +3,10 @@ import { Component,EventEmitter,Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { ValidarService } from '../login-Adm/services/validar-service';
+import { CarrinhoService } from '../Carrinho-compras/services/service-carrinho';
+import { MatDialog } from '@angular/material/dialog';
+import { Alerts } from '../Area-Adm/shared/components/alerts/alerts';
 
 
 @Component({
@@ -14,13 +18,45 @@ import { Router } from '@angular/router';
 })
 export class ProdutoCard {
   @Input() produto!: Produto ;
-    adicionarCarrinho(produto: Produto) {
-      console.log("Cliquei no botão!");
-    }
 
-    constructor(private router: Router) { }
+  constructor(private router: Router,
+    private validarService: ValidarService,
+    private carrinhoService: CarrinhoService,
+    private dialog: MatDialog
+  ) { }
 
     irParaProduto(id: number) {
-    this.router.navigate(['/produto-info', id]);
-  }
+      this.router.navigate(['/produto-info', id]);
+    }
+    // Função para adicionar ao carrinho
+      adicionarAoCarrinho(produto: Produto) {
+        const usuario = this.validarService.getUsuario();
+
+        if (usuario) {
+          this.carrinhoService.adicionarItem(Number(usuario.id), produto.id, 1)
+            .subscribe({
+              next: (response) => {
+                console.log('Produto adicionado ao carrinho', response);
+
+                // 🔥 Aqui abre o aviso de sucesso!
+                this.dialog.open(Alerts, {
+                  data: 'Produto adicionado ao carrinho!',
+                  width: '300px'
+                });
+              },
+              error: (error) => {
+                console.error(error);
+
+                // 🔥 Aviso de erro
+                this.dialog.open(Alerts, {
+                  data: 'Erro ao adicionar ao carrinho.',
+                  width: '300px'
+                });
+              }
+            });
+
+        } else {
+          this.router.navigate(['/login']);
+        }
+      }
 }
